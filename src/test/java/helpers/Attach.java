c70db511c7a9011924eac4a062d4276bbbebaa6a
Natalia Sinitsyna <natalia.a.sinitsyna@gmail.com>
2023-02-17 17:20:22 +0300

    add data for jenkins

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -44,7 +44,7 @@ public class Attach {
     }
 
     public static URL getVideoUrl() {
-        String videoUrl = System.getProperty("video_url") + sessionId() + ".mp4"; //"https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
+        String videoUrl = System.getProperty("videoUrl") + sessionId() + ".mp4"; //"https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
         System.out.println(sessionId());
         try {
             return new URL(videoUrl);
